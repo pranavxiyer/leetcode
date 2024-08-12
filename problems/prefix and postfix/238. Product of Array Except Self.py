1a375cb926e09e9df4c968b106cef1c15931ebd6
pranavxiyer <pranavxiyer@gmail.com>
2024-08-12 14:08:12 -0700

    product of array except self

--- problems/prefix and postfix/238. Product of Array Except Self.py
+++ problems/prefix and postfix/238. Product of Array Except Self.py
@@ -2,3 +2,36 @@
 # https://leetcode.com/problems/product-of-array-except-self/description
 # difficulty: medium
 
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        prefix = []
+        postfix = [1 for i in range(len(nums))]
+        answer = []
+
+        prefix_total = 1
+        for i in range(len(nums)):
+            prefix_total *= nums[i]
+            prefix.append(prefix_total)
+
+        postfix_total = 1
+        for i in range(len(nums) - 1, -1, -1):
+            postfix_total *= nums[i]
+            postfix[i] = postfix_total
+
+        for i in range(len(nums)):
+            if i == 0:
+                left = 1
+                right = postfix[i + 1]
+            elif i == len(nums) - 1:
+                right = 1
+                left = prefix[i - 1]
+            else:
+                left = prefix[i - 1]
+                right = postfix[i + 1]
+            answer.append(left * right)
+        
+        return answer
